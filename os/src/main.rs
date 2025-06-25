293c5f4f5ce902b2e7648b71aa1ab1ab178be51e
mochenya <2827075398@qq.com>
2025-06-25 14:48:18 +0800

    feat: 完成批处理内核系统

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,23 +1,22 @@
 #![no_main]
 #![no_std]
-mod console;
-mod lang_items;
-mod sbi;
 
-use core::arch::global_asm;
+#[macro_use]
+mod console;
+mod batch;
+mod lang_items;
+mod logging;
+mod sbi;
+mod sync;
+pub mod syscall;
+pub mod trap;
+
 use crate::sbi::shutdown;
+use core::arch::global_asm;
+use log::{debug, error, info, trace, warn};
 
 global_asm!(include_str!("entry.asm"));
-
-#[unsafe(no_mangle)]
-fn rust_main() {
-    clear_bss();
-    println!("Hello, RISC-V World!");
-
-    // panic!("Shutdown machine!");
-    shutdown(false);
-    // loop {}
-}
+global_asm!(include_str!("link_app.S"));
 
 fn clear_bss() {
     unsafe extern "C" {
@@ -27,4 +26,46 @@ fn clear_bss() {
     (sbss as usize..ebss as usize).for_each(|a| unsafe {
         (a as *mut u8).write_volatile(0);
     })
+}
+
+#[unsafe(no_mangle)]
+fn rust_main() {
+    unsafe extern "C" {
+        safe fn stext(); // begin addr of text segment
+        safe fn etext(); // end addr of text segment
+        safe fn srodata(); // start addr of Read-Only data segment
+        safe fn erodata(); // end addr of Read-Only data ssegment
+        safe fn sdata(); // start addr of data segment
+        safe fn edata(); // end addr of data segment
+        safe fn sbss(); // start addr of BSS segment
+        safe fn ebss(); // end addr of BSS segment
+        safe fn boot_stack_lower_bound(); // stack lower bound
+        safe fn boot_stack_top(); // stack top
+    }
+    clear_bss();
+    logging::init();
+    println!("[kernel] Hello, RISC-V World!");
+    println!("[kernel] Hello, world!");
+    trace!(
+        "[kernel] .text [{:#x}, {:#x})",
+        stext as usize, etext as usize
+    );
+    debug!(
+        "[kernel] .rodata [{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+    info!(
+        "[kernel] .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+    warn!(
+        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack_lower_bound as usize
+    );
+    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    trap::init();
+    batch::init();
+    batch::run_next_app();
+    // shutdown(false);
+    // loop {}
 }
