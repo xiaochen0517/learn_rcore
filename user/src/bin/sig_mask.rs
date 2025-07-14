c8345fde6ae2460c17c5a9257007d93aa95d39fe
mochenya <2827075398@qq.com>
2025-07-14 09:15:01 +0800

    feat: 信号机制的 mask 测试

---
+++ user/src/bin/sig_mask.rs
@@ -0,0 +1,38 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+extern crate user_lib;
+
+use core::arch::asm;
+use user_lib::*;
+
+fn func(sig: usize) {
+    println!("user_sig_test passed: {}", sig);
+    sigreturn();
+}
+
+#[unsafe(no_mangle)]
+pub fn main() -> i32 {
+    let mut new = SignalAction::default();
+    let mut old = SignalAction::default();
+    new.handler = func as usize;
+
+    println!("signal_simple: sigaction");
+    if sigaction(SIGUSR1, Some(&new), Some(&mut old)) < 0 {
+        panic!("Sigaction failed!");
+    }
+    sigprocmask(u32::MAX);
+    println!("signal_simple: kill");
+    if kill(getpid() as usize, SIGKILL) < 0 {
+        println!("Kill failed!");
+        exit(1);
+    }
+    let mut sig: usize = 0;
+    unsafe {
+        // 使用汇编读取 a0 寄存器
+        asm!("mv {}, a0", out(reg) sig);
+    }
+    println!("signal_simple: sig {}", sig);
+    0
+}
